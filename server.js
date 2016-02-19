5af4d9c2edd8db5e3cfc006eaafba4253985e23b
a34ndju <34ndju@gmail.com>
2016-02-19 03:58:53 +0000

    1

--- server.js
+++ server.js
@@ -4,30 +4,25 @@ var express = require('express');
 var routes = require('./app/routes/index.js');
 var mongoose = require('mongoose');
 var passport = require('passport');
+var path = require('path');
 var session = require('express-session');
 
 var app = express();
-require('dotenv').load();
-require('./app/config/passport')(passport);
 
-mongoose.connect(process.env.MONGO_URI);
+app.use(express.static(path.resolve(__dirname, 'client')));
 
-app.use('/controllers', express.static(process.cwd() + '/app/controllers'));
-app.use('/public', express.static(process.cwd() + '/public'));
-app.use('/common', express.static(process.cwd() + '/app/common'));
+mongoose.connect('mongodb://localhost/base');
 
-app.use(session({
-	secret: 'secretClementine',
-	resave: false,
-	saveUninitialized: true
-}));
+app.get('/', function(req, res) {
+    res.sendFile(process.cwd() + '/client/html/home.html');
+});
 
-app.use(passport.initialize());
-app.use(passport.session());
-
-routes(app, passport);
+app.get('/login', function(req, res) {
+    res.sendFile(process.cwd() + '/client/html/login.html');
+});
 
 var port = process.env.PORT || 8080;
 app.listen(port,  function () {
 	console.log('Node.js listening on port ' + port + '...');
 });
+
