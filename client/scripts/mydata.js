68ed220295c50c326b704dbb2ef464ce7e1f76e6
a34ndju <34ndju@gmail.com>
2016-05-04 03:00:09 +0000

    5/3

--- client/scripts/mydata.js
+++ client/scripts/mydata.js
@@ -1,18 +1,25 @@
 $.getJSON('/mydataAPI', function(data) {
     var files = data.files;
     files.forEach(function(d) {
-        var space = document.createElement("a")
+        var div = document.createElement("div")
+        var space = document.createElement("p")
         var x = document.createElement("a")
+        
+        space.appendChild(document.createTextNode("✕"))
         x.className = "x"
-        x.appendChild(document.createTextNode("✕"))
+        x.appendChild(space)
+        
+        
         var p = document.createElement("p");
+        p.appendChild(document.createTextNode(d.title))
         var link = document.createElement("a");
         link.setAttribute("href", "/product/" + d.id)
-        link.appendChild(document.createTextNode(d.title))
-        p.appendChild(link);
+        link.appendChild(p);
         x.setAttribute("href", "/totalremove/" + d.id)
-        p.appendChild(x)
-        $(".files").append(p)
+        div.appendChild(link)
+        div.appendChild(x)
+        div.className = "box"
+        $(".files").append(div)
     })
 })
 
