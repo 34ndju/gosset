54642b2a838080bdc560b2bfef1b317b1baed1c8
a34ndju <34ndju@gmail.com>
2016-05-09 22:34:35 +0000

    Video Timer

--- client/scripts/home.js
+++ client/scripts/home.js
@@ -1,7 +1,23 @@
-console.log(3)
-
-$("body").click(function() {
-    $('.black').css({'background': 'rgba(0,0,0, 0'});
-    $('.main-header a').css({'color':'white'});
-    $('.videobox').remove()
+$(document).ready(function() {
+    var startTime = new Date().getTime();
+    $("body").click(function() {
+        $('.black').css({'background': 'rgba(0,0,0, 0'});
+        $('.main-header a').css({'color':'white'});
+        $('.videobox').remove()
+        var endTime = new Date().getTime();
+        var timeSpent = endTime - startTime;
+        
+        console.log(timeSpent)
+        
+        ga('send', {
+            hitType: 'timing',
+            timingCategory: 'Video',
+            timingVar: 'Demo Vid Viewtime',
+            timingValue: timeSpent
+        });
+        
+        
+        //window._gaq.push(['_trackTiming', 'Video', 'Video Demo Watch', timeSpent, 'watchTime'])
+    })
 })
+
