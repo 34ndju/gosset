248ff58cee5748e6fec72fb42a6bf392056d083c
a34ndju <34ndju@gmail.com>
2016-05-12 05:22:40 +0000

    AdWords Compliant...Hopefully

--- client/scripts/home.js
+++ client/scripts/home.js
@@ -1,20 +1,42 @@
 $(document).ready(function() {
-    var startTime = new Date().getTime();
-    $("body").click(function() {
-        $('.black').css({'background': 'rgba(0,0,0, 0'});
-        $('.main-header a').css({'color':'white'});
-        $('.videobox').remove()
-        var endTime = new Date().getTime();
-        var timeSpent = endTime - startTime;
-        
-        console.log(timeSpent)
-        
-        ga('send', {
-            hitType: 'timing',
-            timingCategory: 'Video',
-            timingVar: 'Demo Vid Viewtime',
-            timingValue: timeSpent
-        });
-    })
+    var onVid=false;
+    var startTime;
+
+    $(".black, .exit").click(function() {
+        if(onVid) {
+            console.log('onVid');
+            $('.black').height(0);
+            $('.black').width(0);
+            $('.video').attr('src', '');
+            $('.videobox').css('display', 'none')
+            var endTime = new Date().getTime();
+            var timeSpent = endTime - startTime;
+
+            
+            ga('send', {
+                hitType: 'timing',
+                timingCategory: 'Video',
+                timingVar: 'Demo Vid Viewtime',
+                timingValue: timeSpent
+            });
+            
+            onVid = false;
+        }
+    });
+
+
+
+    $(".vid").click(function() {
+        if(!onVid) {
+            console.log('not onVid')
+            $('.video').attr('src', 'https://docs.google.com/file/d/0B_l-CMeRzdw3YjhzajY0dk5EWms/preview?&autoplay=1?');
+            $('.videobox').css('display', 'block');
+            $('.black').height($(document).height());
+            $('.black').width($(document).width());
+            startTime = new Date().getTime();
+            onVid = true;
+        }
+    });
+
 })
 
